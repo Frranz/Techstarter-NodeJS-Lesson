d2f145a4e0e1271e15d4ea5bc0453a43dad726fa
franz <franz@techstarter.de>
2022-11-09 12:15:34 +0100

    add parameter endpoint for == vs === debug

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const secretNumber = 5;
+
 // Hello World
 app.get('/', (request, response) => {
   response.send('Hello World!');
 });
 
-// Debug this (remove an await or change status to statusCode and fuck up url)
+// Debug this (remove an await or change status to statusCode or fuck up url)
 app.get('/catFact', async (request,response) => {
     const httpResponse = await fetch('https://catfact.ninja/fact', {
         method: 'GET',
@@ -23,6 +25,33 @@ app.get('/catFact', async (request,response) => {
     response.send(json.fact)
 })
 
+// irgendwas mit einer sehr langen if mit vielen ors und eins failed
+app.get('/catFact', async (request,response) => {
+  
+});
+
+// irgendwas das unterschied zwischen == und === gut zeigt
+// parameter übergeben und mit zahl vergleichen
+// schlechtes beispiel? Weil eher encouraged nur == zu nehmen? 
+app.get('/guessingGame', async (request,response) => {
+
+  let {name, number}  = request.query;
+
+  let result = "";
+  if (number === secretNumber) {
+    result = "richtig. Herzlichen Glückwunsch";
+  } else {
+    result = "falsch. Versuch es noch einmal";
+  }
+
+  const message = `Teilnehmer ${name} hat auf die Zahl ${number} getippt. Die Zahl ist ` + result;
+
+  response.send(message);
+})
+// irgenwas in textfile/db reinschreiben und direkt wieder abrufen
+
+// irgendwas mit url parameters
+// http://www.omdbapi.com/?s=action&apikey=3a1ff7ce&
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });

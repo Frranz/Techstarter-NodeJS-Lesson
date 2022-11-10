ffc5c46e1950db260235b057a2f5bf33ef6fc278
franz <franz@techstarter.de>
2022-11-10 11:31:40 +0100

    add saveFeedbackToFile exercise

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 const port = 3000;
 
-const secretNumber = 5;
+// necessary to receive formdata on /saveFeedback
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Hello World
 app.get('/', (request, response) => {
@@ -34,8 +37,9 @@ app.get('/catFact', async (request,response) => {
 // parameter übergeben und mit zahl vergleichen
 // schlechtes beispiel? Weil eher encouraged nur == zu nehmen? 
 app.get('/guessingGame', async (request,response) => {
-
   let {name, number}  = request.query;
+
+  const secretNumber = 5;
 
   let result = "";
   if (number === secretNumber) {
@@ -48,7 +52,41 @@ app.get('/guessingGame', async (request,response) => {
 
   response.send(message);
 })
+
 // irgenwas in textfile/db reinschreiben und direkt wieder abrufen
+
+// irgendwas mit split oder so wo es einen array index zu früh abcuttet
+
+
+// send HTML file with feedback form
+app.get('/feedback', (request,response) => {
+  response.sendFile(__dirname + "/index.html");
+});
+
+app.post('/saveFeedbackToFile', async (request,response) => {
+  // define path for file with feedback
+  feedbackPath = __dirname + "/feedbacks.json"
+
+  // get information from html form
+  const {mail, feedbackText} = request.body;
+
+  // create new feedback entry
+  const newFeedback = {
+    mail: mail,
+    feedbackText: feedbackText,
+    date: new Date(),
+  };
+
+  // complete from here on
+  // load json file with feedback
+  // add feedback and save file
+  const feedbacks = await fs.readFile(feedbackPath)
+  const feedbacksJson = JSON.parse(feedbacks);
+
+  feedbacksJson.push(newFeedback);
+  await fs.writeFile(feedbackPath, JSON.stringify(feedbacksJson))
+  response.send("Feedback has been stored");
+});
 
 // irgendwas mit url parameters
 // http://www.omdbapi.com/?s=action&apikey=3a1ff7ce&
